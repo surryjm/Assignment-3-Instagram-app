b1e11c98e3447dd39cbc1406cf726795d298119a
surryjm <50641921+surryjm@users.noreply.github.com>
2020-04-17 22:18:48 -0700

    ProfileReducer

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { GET_ERRORS } from "./types"
+
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,9 @@
 //track all dispatch types/calls the application will listen for
 export const SET_CURRENT_USER = "SET_CURRENT_USER"; //variable for string value
 export const GET_ERRORS = "GET_ERRORS";
+export const GET_PROFILE = "GET_PROFILE";
+export const GET_PROFILES = "GET_PROFILES";
+export const PROFILE_LOADING = "PROFILE_LOADING";
+export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND";
+export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
+
